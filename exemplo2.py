d59ed17d88f90b28f4ac89827f9dfa6262951f88
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2025-04-22 08:42:08 -0300

    AULA 2 - PRINT E VARIAVEIS

---
+++ exemplo2.py
@@ -0,0 +1,9 @@
+nome = "Vanessa"
+idade = 28
+altura = 1.62
+
+print(nome)
+print(idade, altura)
+print("Oi, eu sou", nome)
+print("Minha altura é", altura, "metros.")
+print("Eu tenho", idade, "anos.")
