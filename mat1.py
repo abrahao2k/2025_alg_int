a8090db95a598a85704ac9d2ac7cb878e7a18962
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2025-04-29 10:22:17 -0300

    AULA 3 - OPERAÇÕES MATEMÁTICAS

---
+++ mat1.py
@@ -0,0 +1,11 @@
+# OPERAÇÕES MATEMÁTICAS
+print(5 + 3) # soma
+print(12 - 5) # subtração
+print(2 * 3) # multiplicação
+print(2 ** 4) # potencia (base ** expoente)
+print(15 / 2) # divisão
+print(15 // 2) # quociente (parte inteira da divisão)
+print(15 % 2) # resto da divisao (inteiro)
+
+
+
