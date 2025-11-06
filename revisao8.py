c532a8518a47dae7813f7608d51ee3d3291d563f
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2025-11-06 11:45:20 -0300

    REVISAO PROVA 3o BIM

---
+++ revisao8.py
@@ -0,0 +1,17 @@
+# ORDENAR OS ELEMENTOS DE UMA LISTA (CRESCENTE)
+cores = ["azul","verde","cinza", "branco"]
+cores.sort()
+print(cores)
+
+# ORDEM DECRESCENTE (MAIOR PARA O MENOR)
+numeros = [6, 4, 9, 21, 7, 14, 10, 2, 13]
+numeros.sort(reverse=True)
+print(numeros)
+
+# INVERTER A ORDEM DA LISTA (ESPELHAR)
+cores.reverse()
+print(cores)
+
+# LIMPAR A LISTA (REMOVER TODOS OS ELEMENTOS)
+numeros.clear()
+print(numeros)
